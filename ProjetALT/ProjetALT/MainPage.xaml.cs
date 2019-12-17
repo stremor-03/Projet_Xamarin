fe66fe75b4ff1968994923276a68f08d153b1969
SullyvanT <tremor.sullyvan@gmail.com>
2019-12-17 20:37:06 +0100

    Link data to listView
    Android to debug

--- ProjetALT/ProjetALT/MainPage.xaml.cs
+++ ProjetALT/ProjetALT/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace ProjetALT
             Stream stream = response.GetResponseStream();
             Encoding encode = Encoding.GetEncoding("utf-8");
 
-            List<Message> messages;
+            List<Message> messages = null;
 
             using (StreamReader translatedStream = new StreamReader(stream, encode))
             {
@@ -38,28 +38,11 @@ namespace ProjetALT
                 }
             }
 
-            Label header = new Label
-            {
-                Text = "ListView",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                HorizontalOptions = LayoutOptions.Center
-            };
-
-            // Define some data.
-            List<Person> people = new List<Person>
-            {
-                new Person("Abigail", new DateTime(1975, 1, 15), Color.Aqua),
-                new Person("Bob", new DateTime(1976, 2, 20), Color.Black),
-                // ...etc.,...
-                new Person("Yvonne", new DateTime(1987, 1, 10), Color.Purple),
-                new Person("Zachary", new DateTime(1988, 2, 5), Color.Red)
-            };
-
             // Create the ListView.
             ListView listView = new ListView
             {
                 // Source of data items.
-                ItemsSource = people,
+                ItemsSource = messages,
 
                 // Define template for displaying each item.
                 // (Argument of DataTemplate constructor is called for 
@@ -67,72 +50,49 @@ namespace ProjetALT
                 ItemTemplate = new DataTemplate(() =>
                 {
                     // Create views with bindings for displaying each property.
-                    Label nameLabel = new Label();
-                    nameLabel.SetBinding(Label.TextProperty, "Name");
+                    Label studentIDLabel = new Label();
+                    studentIDLabel.SetBinding(Label.TextProperty, "Student_id");
 
-                    Label birthdayLabel = new Label();
-                    birthdayLabel.SetBinding(Label.TextProperty,
-                        new Binding("Birthday", BindingMode.OneWay,
-                            null, null, "Born {0:d}"));
-
-                    BoxView boxView = new BoxView();
-                    boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+                    Label messageLabel = new Label();
+                    messageLabel.SetBinding(Label.TextProperty,"Student_message");
 
                     // Return an assembled ViewCell.
                     return new ViewCell
                     {
                         View = new StackLayout
                         {
-                            Padding = new Thickness(0, 5),
-                            Orientation = StackOrientation.Horizontal,
+                            VerticalOptions = LayoutOptions.Center,
+                            Spacing = 0,
                             Children =
-                                {
-                                    boxView,
-                                    new StackLayout
-                                    {
-                                        VerticalOptions = LayoutOptions.Center,
-                                        Spacing = 0,
-                                        Children =
-                                        {
-                                            nameLabel,
-                                            birthdayLabel
-                                        }
-                                        }
-                                }
+                            {
+                                studentIDLabel,
+                                messageLabel
+                            }
                         }
                     };
                 })
             };
 
-            // Accomodate iPhone status bar.
-            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(10, 20, 10, 5);
+                    break;
+                default:
+                    this.Padding = new Thickness(10, 0, 10, 5);
+                    break;
+            }
 
             // Build the page.
             this.Content = new StackLayout
             {
                 Children =
                 {
-                    header,
                     listView
                 }
             };
         }
     }
-
-    class Person
-    {
-        public Person(string name, DateTime birthday, Color favoriteColor)
-        {
-            this.Name = name;
-            this.Birthday = birthday;
-            this.FavoriteColor = favoriteColor;
-        }
-
-        public string Name { private set; get; }
-
-        public DateTime Birthday { private set; get; }
-
-        public Color FavoriteColor { private set; get; }
-    };
 }
 
